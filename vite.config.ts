9f102d51c7c7efa982bfa7bc5a1721f5d27e79f6
KaranMurari <162008615+KaranMurari@users.noreply.github.com>
2025-09-02 14:23:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import sitemap from 'vite-plugin-sitemap' // 1. IMPORT THE PLUGIN
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({ hostname: 'https://maneet.vercel.app' }), // 2. ADD THE PLUGIN WITH YOUR URL
+    sitemap({ 
+      hostname: 'https://maneet.vercel.app',
+      robots: [{ userAgent: '*', allow: '/' }] // <-- ADD THIS LINE
+    }),
   ],
   resolve: {
     alias: {
